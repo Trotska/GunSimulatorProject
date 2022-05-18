32a476ab0d3ddc99dcc51bc186f6f2120dfa6990
Trotska <lagiu4@gmail.com>
2022-05-19 02:47:32 +1000

    Update Turret.cs
    Slight update to turret, not finished yet

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -9,10 +9,14 @@ public class Turret : MonoBehaviour
     private GameObject target;
     private bool targetLocked;
 
+    public Transform castPoint;
+
     public GameObject turret;
     public GameObject bullet;
     public float fireTimer;
     private bool shotready;
+
+    private bool seen = false;
     void Start()
     {
         
@@ -42,6 +46,36 @@ public class Turret : MonoBehaviour
             targetLocked = true;
         }
 
+    }
+
+    bool PlayerWithinDistance(float distance)
+    {
+        if (Vector3.Distance(target.transform.position, transform.position) < distance)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool CanSeePlayer(float distance)
+    {
+        bool val = false;
+        var castDist = distance;
+
+        Vector3 playerDirection = (target.transform.position - gameObject.transform.position);
+        RaycastHit rayhit;
+        //Vector3 endpos = gameObject.p;
+        //RaycastHit hit = Physics.Linecast(transform.position, transform.rotation);
+        if (Physics.Raycast(castPoint.position, playerDirection, out rayhit, distance))
+        {
+            if (rayhit.collider.CompareTag("Player"))
+            {
+                return true;
+            }
+
+
+        }
+        return false;
     }
 
 
